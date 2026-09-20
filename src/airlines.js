c40d4397456ecc0d6bda9cecd74f05a07d7d160a
fwragenciadigital <34317193+fwragenciadigital@users.noreply.github.com>
2026-09-20 08:45:32 -0300

    Add src/airlines.js

---
+++ src/airlines.js
@@ -0,0 +1,50 @@
+export const airlines = {
+  iberia: {
+    name: "Iberia",
+    code: "IB",
+    bookingUrl: "https://www.iberia.com/",
+    status: "connector_pending"
+  },
+  "air-europa": {
+    name: "Air Europa",
+    code: "UX",
+    bookingUrl: "https://www.aireuropa.com/",
+    status: "connector_pending"
+  },
+  "air-france": {
+    name: "Air France",
+    code: "AF",
+    bookingUrl: "https://wwws.airfrance.it/",
+    status: "connector_pending"
+  },
+  klm: {
+    name: "KLM",
+    code: "KL",
+    bookingUrl: "https://www.klm.it/",
+    status: "connector_pending"
+  },
+  tap: {
+    name: "TAP Air Portugal",
+    code: "TP",
+    bookingUrl: "https://www.flytap.com/",
+    status: "validation_ready"
+  },
+  latam: {
+    name: "LATAM",
+    code: "LA",
+    bookingUrl: "https://www.latamairlines.com/it/it",
+    status: "connector_pending"
+  },
+  lufthansa: {
+    name: "Lufthansa",
+    code: "LH",
+    bookingUrl: "https://www.lufthansa.com/it/it/homepage",
+    status: "connector_pending"
+  },
+  ita: {
+    name: "ITA Airways",
+    code: "AZ",
+    bookingUrl: "https://www.ita-airways.com/it_it",
+    status: "connector_pending"
+  }
+};
